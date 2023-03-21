ed54ea37ce5933da1b3a6a0fde34474b3d4890f9
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-03-21 09:05:27 +0100

    Add location to improve ngIf propriety

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,34 +4,38 @@ import { FaceSnap } from './models/facesnap.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  mySnap!:FaceSnap;
-  myOtherSnap!:FaceSnap;
-  myLastSnap!:FaceSnap;
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
-  ngOnInit(): void {
-    this.mySnap=new FaceSnap(
-      'Jean-Louis Aubert',
-      'Mon chanteur préféré',
-      'https://www.jeanlouisaubert.com/var/warner/storage/images/1/5/5/8/2198551-1-fre-FR/rub-photo-1.jpg',
-      new Date(),
-      0
-    )
-    this.myOtherSnap=new FaceSnap(
-      'Olivier Adam',
-      'Mon auteur préférée',
-      'https://www.lexpress.fr/resizer/TaL0kaf_CvFId7d-Rdstav_IsXo=/970x548/cloudfront-eu-central-1.images.arcpublishing.com/lexpress/7BF7R5IA2FG65GHZVOMMXW7H74.jpg',
-      new Date(),
-      0
-    )
-    this.myLastSnap=new FaceSnap(
-      'Vincent Macaigne',
-      'Mon acteur préféré',
-      'https://www.babelio.com/users/AVT_Vincent-Macaigne_8369.jpg',
-      new Date(),
-      0
-    )
+  ngOnInit() {
+    (this.mySnap = {
+      title: 'Jean-Louis Aubert',
+      description: 'Mon chanteur préféré',
+      imageUrl:
+        'https://www.jeanlouisaubert.com/var/warner/storage/images/1/5/5/8/2198551-1-fre-FR/rub-photo-1.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Paris',
+    }),
+      (this.myOtherSnap = {
+        title: 'Olivier Adam',
+        description: 'Mon auteur préférée',
+        imageUrl:
+          'https://www.lexpress.fr/resizer/TaL0kaf_CvFId7d-Rdstav_IsXo=/970x548/cloudfront-eu-central-1.images.arcpublishing.com/lexpress/7BF7R5IA2FG65GHZVOMMXW7H74.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+      }),
+      (this.myLastSnap = {
+        title: 'Vincent Macaigne',
+        description: 'Mon acteur préféré',
+        imageUrl: 'https://www.babelio.com/users/AVT_Vincent-Macaigne_8369.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Tonnerre',
+      });
   }
 }
--- src/app/models/facesnap.model.ts
+++ src/app/models/facesnap.model.ts
@@ -1,11 +1,10 @@
 export class FaceSnap {
-  constructor(
-    public title: string,
-    public description: string,
-    public imageUrl: string,
-    public createdDate: Date,
-    public snaps: number
-  ) {}
+  title!: string;
+  description!: string;
+  imageUrl!: string;
+  createdDate!: Date;
+  snaps!: number;
+  location?: string;
 }
 
 //   Même chose que :
