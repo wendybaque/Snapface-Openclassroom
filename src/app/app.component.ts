162c948b5d50ba446e7a3a4a3bbe6a47edf50f2c
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-03-21 09:45:56 +0100

    Add style to improve ngStyle directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
         imageUrl:
           'https://www.jeanlouisaubert.com/var/warner/storage/images/1/5/5/8/2198551-1-fre-FR/rub-photo-1.jpg',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 241,
         location: 'Paris',
       },
       {
@@ -26,14 +26,14 @@ export class AppComponent implements OnInit {
         imageUrl:
           'https://www.lexpress.fr/resizer/TaL0kaf_CvFId7d-Rdstav_IsXo=/970x548/cloudfront-eu-central-1.images.arcpublishing.com/lexpress/7BF7R5IA2FG65GHZVOMMXW7H74.jpg',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 143,
       },
       {
         title: 'Vincent Macaigne',
         description: 'Mon acteur préféré',
         imageUrl: 'https://www.babelio.com/users/AVT_Vincent-Macaigne_8369.jpg',
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 100,
         location: 'Tonnerre',
       },
     ];
