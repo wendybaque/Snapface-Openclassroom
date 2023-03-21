515863959a3b204127018a05978a72a13b28ed0d
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-03-21 09:19:44 +0100

    Add array to improve ngFor propriety

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,35 @@ import { FaceSnap } from './models/facesnap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    (this.mySnap = {
-      title: 'Jean-Louis Aubert',
-      description: 'Mon chanteur préféré',
-      imageUrl:
-        'https://www.jeanlouisaubert.com/var/warner/storage/images/1/5/5/8/2198551-1-fre-FR/rub-photo-1.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Paris',
-    }),
-      (this.myOtherSnap = {
+    this.faceSnaps = [
+      {
+        title: 'Jean-Louis Aubert',
+        description: 'Mon chanteur préféré',
+        imageUrl:
+          'https://www.jeanlouisaubert.com/var/warner/storage/images/1/5/5/8/2198551-1-fre-FR/rub-photo-1.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris',
+      },
+      {
         title: 'Olivier Adam',
         description: 'Mon auteur préférée',
         imageUrl:
           'https://www.lexpress.fr/resizer/TaL0kaf_CvFId7d-Rdstav_IsXo=/970x548/cloudfront-eu-central-1.images.arcpublishing.com/lexpress/7BF7R5IA2FG65GHZVOMMXW7H74.jpg',
         createdDate: new Date(),
         snaps: 0,
-      }),
-      (this.myLastSnap = {
+      },
+      {
         title: 'Vincent Macaigne',
         description: 'Mon acteur préféré',
         imageUrl: 'https://www.babelio.com/users/AVT_Vincent-Macaigne_8369.jpg',
         createdDate: new Date(),
         snaps: 0,
         location: 'Tonnerre',
-      });
+      },
+    ];
   }
 }
