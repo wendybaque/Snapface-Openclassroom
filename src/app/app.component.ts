1e221ec68e147b83923b104012ea3207e447aa97
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-05-22 14:03:50 +0200

    Opérateurs de bas niveau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Observable, filter, interval, map, tap } from 'rxjs';
+
+// map : transforme les émissions d'un observable.
+// filter : filtre les émission d'un observable.
+// tap : ajoute des effets secondaires/side effects à un observable.
 
 @Component({
   selector: 'app-root',
@@ -7,8 +11,22 @@ import { Observable, interval } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  interval$!:Observable<number>;
+  interval$!: Observable<string>;
   ngOnInit() {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      // L'ordre des opérateurs est important !
+      filter((value) => value % 3 === 0),
+      map((value) =>
+        value % 2 === 0
+          ? `Je suis ${value} et je suis pair`
+          : `Je suis ${value} et je suis impair`
+      ),
+      tap((text) => this.logger(text))
+    );
+  }
+
+  // effet secondaire ou side effect
+  logger(text: string) {
+    console.log(`log: ${text}`);
   }
 }
