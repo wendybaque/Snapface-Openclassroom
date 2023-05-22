aa78a22e8085055880104e805be2941eada059f5
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-05-22 14:44:46 +0200

    Cours 4 éviter les fuites de mémoire

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FaceSnap } from '../models/facesnap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { interval, take, tap, Subject, takeUntil } from 'rxjs';
+// Subject : observable qu'on peut faire émettre quand on veut.
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss'],
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000)
+      .pipe(
+        // Quand on veut prendres des valeurs d'émissions jusqu'à un nombre donné :
+        takeUntil(this.destroy$),
+        // Quand on sait en amont combien on veut d'émissions précisément :
+        // take(5),
+        tap(console.log)
+      )
+      .subscribe();
+  }
+
+  // Pour détruire le composant :
+  ngOnDestroy() {
+    this.destroy$.next(true);
   }
 }
