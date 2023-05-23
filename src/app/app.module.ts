b14f171fbb71c460715070b706ddb3112eccda43
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-05-23 12:47:44 +0200

    Cours 5 template forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
@@ -10,7 +11,6 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignleFaceSnapComponent } from './signle-face-snap/signle-face-snap.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +20,7 @@ import { SignleFaceSnapComponent } from './signle-face-snap/signle-face-snap.com
     LandingPageComponent,
     SignleFaceSnapComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
 })
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +8,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  userEmail!: string;
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   onContinue() {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm): void {
+    console.log(form.value);
   }
 }
